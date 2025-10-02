054345145227131bf58ade21c15aa60925abb1d0
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-02 14:54:52 +0000

    No you have to do it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ export default {
         body: ['PT Sans', 'sans-serif'],
         headline: ['PT Sans', 'sans-serif'],
         code: ['monospace'],
+        jarvis: ['Audiowide', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
