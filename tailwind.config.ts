a89c4c0654dd04dbc208808dd77d7df569896348
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-02 14:56:05 +0000

    No orbiton is better use it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
         body: ['PT Sans', 'sans-serif'],
         headline: ['PT Sans', 'sans-serif'],
         code: ['monospace'],
-        jarvis: ['Audiowide', 'sans-serif'],
+        jarvis: ['Orbitron', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
