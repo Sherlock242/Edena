69a8b9db44910bd4a2a1122b5c7cc9e04a5cb716
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-02 20:51:37 +0000

    It's not showing

--- src/app/edena.tsx
+++ src/app/edena.tsx
@@ -303,7 +303,7 @@ const AIConsciousnessPage = () => {
                   )}
                 </AnimatePresence>
               </div>
-              <Button>
+              <Button className="h-10 w-10">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="menu-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
@@ -410,3 +410,5 @@ const AIConsciousnessPage = () => {
 };
 
 export default AIConsciousnessPage;
+
+    
