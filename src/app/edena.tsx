a043fbebd958987e6bccd69483b337c374394757
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-03 09:54:48 +0000

    Make sure side menu appers from right side

--- src/app/edena.tsx
+++ src/app/edena.tsx
@@ -376,14 +376,14 @@ const menuIconColor = isImageMode ? 'orangered' : 'cyan';
                         <Menu style={{ color: menuIconColor }} />
                     </Button>
                 </SheetTrigger>
-                <SheetContent side="left" className="w-1/2 bg-transparent border-0 shadow-none p-8 flex flex-col justify-center">
+                <SheetContent side="right" className="w-1/2 bg-transparent border-0 shadow-none p-8 flex flex-col justify-center">
                   <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
                     <div className="flex flex-col space-y-8">
-                        <Button variant="ghost" className="text-2xl h-20 text-white hover:bg-white/10" onClick={() => handleModeChange('search')}>
+                        <Button variant="ghost" className="text-2xl h-20 text-cyan-400 hover:bg-white/10" onClick={() => handleModeChange('search')}>
                             <Search className="mr-4 h-8 w-8" />
                             <span>Search Edena</span>
                         </Button>
-                        <Button variant="ghost" className="text-2xl h-20 text-white hover:bg-white/10" onClick={() => handleModeChange('image')}>
+                        <Button variant="ghost" className="text-2xl h-20 text-orange-500 hover:bg-white/10" onClick={() => handleModeChange('image')}>
                             <ImageIcon className="mr-4 h-8 w-8" />
                             <span>Image Edena</span>
                         </Button>
@@ -499,5 +499,3 @@ const menuIconColor = isImageMode ? 'orangered' : 'cyan';
 };
 
 export default AIConsciousnessPage;
-
-    
