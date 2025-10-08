f4d4f7af1a0421418c1ab0677aa30b15235fa9c2
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-08 04:56:20 +0000

    Still not in center

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,14 +15,13 @@ export default function AdminPage() {
             <ArrowLeft />
           </Button>
         </div>
-        <div className="justify-self-center">
+        <div className="justify-self-center col-start-2">
           <h1 className="text-2xl font-jarvis text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500 whitespace-nowrap">
             Admin Panel
           </h1>
         </div>
-        <div />
       </header>
-      <main className="flex-1 flex flex-col items-center justify-center text-center">
+      <main className="flex-1 flex flex-col items-center justify-center text-center -mt-16">
         <div className="w-full max-w-sm">
           <HologramEffect />
         </div>
