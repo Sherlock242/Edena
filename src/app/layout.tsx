86745c60761a75f04e025a5573d9c9bbc56a2ce8
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-11-13 15:24:27 +0000

    I want to add ads in my site so place this script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
@@ -22,6 +23,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script type='text/javascript' src='//certainwolveshonestly.com/e3/6d/06/e36d068ab2ed1fe0979437ad7a7d4e6a.js' />
+        <Script type='text/javascript' src='//certainwolveshonestly.com/15/a8/0e/15a80e7627074b2defc9f1024108d085.js' />
       </body>
     </html>
   );
